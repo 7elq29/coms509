620a491a70221f1a3222f69753f88d6a9409897a
Renchang Miao <mrc12345678@gmail.com>
2016-11-28 15:33:44 -0600

    add record

--- server/home.py
+++ server/home.py
@@ -2,10 +2,13 @@ from flask import Flask, request
 from flask import Response
 from flaskext.mysql import MySQL
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 import json
+import datetime
 
 app = Flask(__name__)
 mysql = MySQL()
+CORS(app)
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
@@ -17,23 +20,21 @@ mysql.init_app(app)
 
 @app.route('/')
 def hello_world():
-	cur = mysql.connect().cursor()
-	# cur.execute('''select * from test, merit, patient where test.MRID = patient.MRID and test.MID = merit.MID''')
-	rv = cur.fetchall()
-	return str(rv)
+	return "server side"
 
 @app.route('/login')
 def longinValid():
-	return ""
+	res = {"status":"success","message":"longin success"}
+	return jsonify(**res)
 
 @app.route('/search', methods=['GET', 'POST'])
 def searchData():
 	if request.method == 'POST':
 		data = request.data
 		dataDict = json.loads(data)
-		keyWord = "%Ann%"
+		keyWord = dataDict["keyword"]
 		cur = mysql.connect().cursor()
-		cur.execute("select patient.PNAME, patient.MRID from test, merit, patient where test.MRID = patient.MRID and test.MID = merit.MID and patient.`PNAME` like %s", [keyWord])
+		cur.execute("select patient.PNAME, patient.MRID from patient where patient.`PNAME` like %s", [keyWord])
 		rv = cur.fetchall();
 		resList = []
 		for row in rv:
@@ -45,16 +46,23 @@ def searchData():
 		res = {'result':{}}
 		return jsonify(**res)
 
-@app.route('/searchByMR',methods=['GET', 'POST'])
-def searchByMR():
+@app.route('/searchByMR/<int:mrid>',methods=['GET', 'POST'])
+def searchByMR(mrid):
 	if request.method == 'GET':
-		mrId = '2'
-		# startTime = '2016-1-1'
-		# endTime = '2017-2-2' 
+		print "mrid:"+str(mrid)
+		startTime = request.args.get("starttime")
+		endTime = request.args.get("endtime")
 		cur = mysql.connect().cursor()
-		# cur.execute("select * from test, merit, patient where test.MRID = patient.MRID and test.MID = merit.MID and patient.MRID = %s and test.DATE >=%s and test.DATE<=%s",[mrId,startTime,endTime])
-		cur.execute("select * from test, merit, patient where test.MRID = patient.MRID and test.MID = merit.MID and patient.MRID = %s and test.DATE >=%s and test.DATE<=%s",[mrId,startTime,endTime])
+		if startTime and endTime:
+			startT = datetime.datetime.fromtimestamp(float(startTime))
+			endT = datetime.datetime.fromtimestamp(float(endTime))
+			cur.execute("select * from test, merit, patient where test.MRID = patient.MRID and test.MID = merit.MID and patient.MRID = %s and test.DATE >=%s and test.DATE<=%s",[mrid,startT,endT])
+		else :
+			cur.execute("select * from test, merit, patient where test.MRID = patient.MRID and test.MID = merit.MID and patient.MRID = %s ",[mrid])
 		rv = cur.fetchall();
+		if(len(rv) == 0) :
+			return Response(status=404)
+
 		patientName = ""
 		ownerName = ""
 		resList = []
@@ -63,15 +71,14 @@ def searchByMR():
 			ownerName = row[13]
 			item = {'testAbbr':row[7],'unit':row[8],'value':row[4],'time':row[2]}
 			resList.append(item)
-		res = {'MRNo':mrId,'patientName':patientNme,'ownerName':ownerName,'tests':resList}
+		res = {'MRNo':mrid,'patientName':patientNme,'ownerName':ownerName,'tests':resList}
 		return jsonify(**res)
 	else: 
 		return Response(status=404)
 
-@app.route('/add',methods=['GET', 'POST','PUT'])
-def addRecord():
+@app.route('/add/<int:mrid>',methods=['GET', 'POST','PUT'])
+def addRecord(mrid):
 	if request.method == 'PUT':
-		mrid = '1'
 		data = request.data
 		dataDict = json.loads(data)
 		dataList = dataDict["test"]
@@ -82,7 +89,9 @@ def addRecord():
 		if count[0] < 1:
 			return Response(status=404)
 		for row in dataList:
-			cur.execute("insert into test(MRID,MID,TESTVALUE,`DATE`) values(%s,%s,%s,%s)",[mrid,row["mid"],row["value"],row["time"]])
+			
+			dateTime = datetime.datetime.fromtimestamp(float(row["time"]/1000))
+			cur.execute("insert into test(MRID,MID,TESTVALUE,`DATE`) values(%s,%s,%s,%s)",[mrid,row["mid"],row["value"],dateTime])
 			con.commit()
 			res = cur.fetchone()
 		res = {"status":"success","message":""}
@@ -95,10 +104,10 @@ def addRecord():
 @app.route('/update',methods=['GET', 'POST'])
 def updateRecord():
 	if request.method == 'POST':
-		mrid = '1'
 		data = request.data
 		dataDict = json.loads(data)
 		dataList = dataDict["test"]
+		mrid = dataDict["MRNo"]
 		con = mysql.get_db()
 		cur = con.cursor()
 		cur.execute("select count(*) from patient where patient.MRID = %s",[mrid])
@@ -106,6 +115,7 @@ def updateRecord():
 		if count[0] < 1:
 			return Response(status=404)
 		for row in dataList:
+			print str(row)
 			cur.execute("update test set test.TESTVALUE = %s where test.MRID = %s and test.MID = %s and test.DATE = %s",[row["value"],mrid,row["mid"],row["time"]])
 			con.commit()
 		res = {"status":"success","message":""}
@@ -113,6 +123,22 @@ def updateRecord():
 	else :
 		return Response(status=404)
 
+@app.route('/addpatient',methods=['POST'])
+def addPatient(mrid):
+	if request.method == 'POST':
+		data = request.data
+		dataDict = json.loads(data)
+		mrid = dataDict["MRNo"]
+		name = dataDict["name"]
+		owner = dataDict["owner"]
+		con = mysql.get_db()
+		cur = con.cursor()
+		if mrid and name and owner:
+			cur.execute("insert into `patient`(MRID,PNAME,WNAME)values(%s,%s,%s)",[mrid,name,owner])
+			con.commit()
+		res = {"status":"success","message":""}
+		return jsonify(res)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host='10.26.14.12')
+	# app.run()
